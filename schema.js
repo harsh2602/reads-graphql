dbc65c6627fb839bd5bbc61a2768b7df2a9673e4
Harsh Khandelwal <harshkhandelwal@Harshs-MacBook-Pro.local>
2017-10-10 00:33:38 -0500

    defined authors inside BookType

--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const util = require('util');
 const parseXML = util.promisify(require('xml2js').parseString);
+const DataLoader = require('dataloader');
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -11,14 +12,19 @@ const {
 } = require('graphql');
 const keys = require('./keys');
 
-function translateLang(lang, str) {
+const translateLang = (lang, str) => {
   // Google Translate API is a paid (but dirt cheap) service.
   // To generate your own API key, go here: https://cloud.google.com/translate/v2/getting_started
   const url = `https://www.googleapis.com/language/translate/v2?key=${keys.googleTransalateApiKey}&source=en&target=${lang}&q=encodeURIComponent(${str})`;
   return fetch(url)
     .then(response => response.json())
     .then(parsedResponse => parsedResponse.data.translations[0].translatedText);
-}
+};
+
+const fetchAuthor = id =>
+  fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=${keys.apiKey}`)
+    .then(response => response.text())
+    .then(parseXML);
 
 const BookType = new GraphQLObjectType({
   name: 'Books',
@@ -28,10 +34,10 @@ const BookType = new GraphQLObjectType({
       type: GraphQLString,
       args: {
         lang: { type: GraphQLString }
-      }
+      },
       resolve: (xml, args) => {
-        const title = xml.GoodreadsResponse.book[0].title[0]
-        return args.lang ? translateLang(args.lang, title) : title
+        const title = xml.GoodreadsResponse.book[0].title[0];
+        return args.lang ? translateLang(args.lang, title) : title;
       }
     },
     isbn: {
@@ -41,6 +47,14 @@ const BookType = new GraphQLObjectType({
     rating: {
       type: GraphQLFloat,
       resolve: xml => xml.GoodreadsResponse.book[0].average_rating[0]
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve: xml => {
+        const authorElements = xml.GoodreadsResponse.book[0].authors[0].author;
+        const ids = authorElements.map(elem => elem.id[0]);
+        return Promise.all(ids.map(id => fetchAuthor(id)));
+      }
     }
   })
 });
@@ -84,12 +98,7 @@ module.exports = new GraphQLSchema({
         args: {
           id: { type: GraphQLInt }
         },
-        resolve: (root, args) =>
-          fetch(
-            `https://www.goodreads.com/author/show.xml?id=${args.id}&key=${keys.apiKey}`
-          )
-            .then(response => response.text())
-            .then(parseXML)
+        resolve: (root, args) => fetchAuthor(args.id)
       }
     })
   })
